9aa5b9135a985a3050d4963e5b7372de4cd8cb86
kanguis <127274002+kanguis@users.noreply.github.com>
2023-03-09 22:16:09 +0700

    Add files via upload

---
+++ js/mimd-feed.js
@@ -0,0 +1,21 @@
+var multiFeed = {
+    feedsUri: [
+        {
+            name: "ARTIKEL TERBARU",
+            url: "https://kang-ismet.com/",
+            tag: "Javascript"
+        }
+    ],
+    numPost: 6,
+    showThumbnail: true,
+    showSummary: false,
+    summaryLength: 30,
+    titleLength: "auto",
+    thumbSize: 400,
+    newTabLink: true,
+    containerId: "feed-list-container",
+    readMore: {
+        text: "Selengkapnya",
+        endParam: "?max-results=15"
+    }
+};
