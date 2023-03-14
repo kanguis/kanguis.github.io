fddbe3a93a4c742b4cb7954283cad49fac056c8a
kanguis <127274002+kanguis@users.noreply.github.com>
2023-03-15 02:17:23 +0700

    Add files via upload

--- js/badwords.js
+++ js/badwords.js
@@ -2,4 +2,7 @@ var comt = document.getElementsByClassName('comment-content');
   for(var i=0;i<comt.length;i++){
        comt[i].innerHTML = comt[i].innerHTML.replace(/anjing|asu|tai|goblok|goblog|sia|brengsek|memek|kontol|monyet|kotor|hoaaxx|janc|ass|hoax|fuck|pantek|porn|qontol|bodoh|gila|shit|manuk|ngentot|setan|iblis|dongo|bangsat|bedebah|cemen|kuntilanak|anji|matamu|jomblo|cok|jancok|babi|kadal|bunting|kecoa|nonggeng|entot|bego|tolol|sempak|sundel|bolong/gi, "**istigfar**  ");
 }   
-//]]>
+var widg = document.getElementsByClassName('widget-content');
+  for(var i=0;i<widg.length;i++){
+       widg[i].innerHTML = widg[i].innerHTML.replace(/anjing|asu|tai|goblok|goblog|sia|brengsek|memek|kontol|monyet|kotor|hoaaxx|janc|ass|hoax|fuck|pantek|porn|qontol|bodoh|gila|shit|manuk|ngentot|setan|iblis|dongo|bangsat|bedebah|cemen|kuntilanak|anji|matamu|jomblo|cok|jancok|babi|kadal|bunting|kecoa|nonggeng|entot|bego|tolol|sempak|sundel|bolong/gi, "**istigfar**  ");
+}
