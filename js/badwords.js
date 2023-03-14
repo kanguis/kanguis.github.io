770be1fa7be1f23b375fa464849fea81cba104b4
kanguis <127274002+kanguis@users.noreply.github.com>
2023-03-15 02:09:05 +0700

    Add files via upload

--- js/badwords.js
+++ js/badwords.js
@@ -1,3 +1,5 @@
-var spamer = document.getElementsByClassName('comment-content');   for(var i=0;i<spamer.length;i++){
-       spamer[i].innerHTML = comt[i].innerHTML.replace(/anjeng|asu|tai|goblok/gi, "**Astagfirulloh**");
-}  
+var comt = document.getElementsByClassName('comment-content');
+  for(var i=0;i<comt.length;i++){
+       comt[i].innerHTML = comt[i].innerHTML.replace(/anjing|asu|tai|goblok|goblog|sia|brengsek|memek|kontol|monyet|kotor|hoaaxx|janc|ass|hoax|fuck|pantek|porn|qontol|bodoh|gila|shit|manuk|ngentot|setan|iblis|dongo|bangsat|bedebah|cemen|kuntilanak|anji|matamu|jomblo|cok|jancok|babi|kadal|bunting|kecoa|nonggeng|entot|bego|tolol|sempak|sundel|bolong/gi, "**istigfar**  ");
+}   
+//]]>
